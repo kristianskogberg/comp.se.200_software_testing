da64e8473141db61726885e699846ce7ac8ca0fa
Kristian Skogberg <k.skogberg98@gmail.com>
2023-11-22 14:47:30 +0200

    update unit tests for add and divide and uncomment failing tests

--- test/add.test.js
+++ test/add.test.js
@@ -2,72 +2,48 @@ import chai from "chai";
 import add from "../src/add.js";
 const expect = chai.expect;
 
-describe("add with number inputs", () => {
-  it("adds two positive numbers", () => {
+describe("add", () => {
+  it("adds two numbers", () => {
     expect(add(6, 4)).to.equal(10);
   });
 
-  it("adds two negative numbers", () => {
-    expect(add(-2, -2)).to.equal(-4);
+  it("returns 0 when both arguments are undefined", () => {
+    expect(add(undefined, undefined)).to.equal(0); // Default value is 0
   });
 
-  it("adds a positive number and a negative number", () => {
-    expect(add(5, -3)).to.equal(2);
+  it("returns the first argument when second argument is undefined", () => {
+    expect(add(6, undefined)).to.equal(6);
   });
 
-  it("adds a number to zero", () => {
-    expect(add(5, 0)).to.equal(5);
+  it("returns the second argument when first argument is undefined", () => {
+    expect(add(undefined, 4)).to.equal(4);
   });
 
-  it("adds zero to a number", () => {
-    expect(add(0, 5)).to.equal(5);
-  });
-
-  it("adds two zeros", () => {
+  it("adds zero correctly", () => {
     expect(add(0, 0)).to.equal(0);
   });
 
-  it("adds floating point numbers", () => {
-    expect(add(1.5, 2.3)).to.be.closeTo(3.8, 0.0001);
-  });
-});
-
-describe("add with non-number inputs", () => {
-  /* When dealing with non-number inputs, the add function should either
-  throw an error or coerce the inputs to numbers, depending on its intended behavior
-  */
-
-  /*
-  it("adds a number and a string representation of a number", () => {
-    expect(add(5, "2")).to.equal(7);
-  });
-
-  it("returns NaN when adding a number and a non-numeric string", () => {
-    expect(add(5, "abc")).to.be.NaN;
-  });
-
-  it("returns NaN when adding two non-numeric strings", () => {
-    expect(add("abc", "def")).to.be.NaN;
-  });
-
-  it("returns NaN when adding a number and undefined", () => {
-    expect(add(1, undefined)).to.be.NaN;
-  });
-
-  it("adds a number and an array with one numeric element", () => {
-    expect(add(1, [2])).to.equal(3);
-  });
-  */
-
-  it("returns NaN when adding a number and an array with non-numeric element", () => {
-    expect(add(1, ["a"])).to.be.NaN;
+  it("adds a number and null (assuming null is coerced to 0)", () => {
+    expect(add(6, null)).to.equal(6);
   });
 
   it("returns NaN when adding a number and an object", () => {
-    expect(add(1, {})).to.be.NaN;
+    expect(add(6, {})).to.be.NaN;
   });
 
-  it("returns NaN when adding a number and null", () => {
-    expect(add(1, null)).to.equal(1); // Assuming null is coerced to 0
+  it("converts string arguments to numbers before addition", () => {
+    expect(add("6", "4")).to.equal(10);
+  });
+
+  it("handles addition with a string and a number", () => {
+    expect(add("6", 4)).to.equal(10);
+  });
+
+  it("returns NaN when adding non-coercible strings", () => {
+    expect(add("a", "b")).to.be.NaN;
+  });
+
+  it("adds a number and an empty string (assuming empty string is coerced to 0)", () => {
+    expect(add(6, "")).to.equal(6);
   });
 });
--- test/divide.test.js
+++ test/divide.test.js
@@ -5,29 +5,39 @@ const expect = chai.expect;
 // NOTE: divide-function is incorrect, it divides the divisor with itself instead of using the divident
 
 describe("divide", () => {
-  /*
   it("divides two positive numbers", () => {
-    expect(divide(6, 4)).to.equal(1.5);
-  });
-
-  it("divides a positive number by a negative number", () => {
-    expect(divide(6, -3)).to.equal(-2);
+    expect(divide(6, 3)).to.equal(2);
   });
 
   it("divides two negative numbers", () => {
-    expect(divide(-6, -3)).to.equal(2);
+    expect(divide(-4, -2)).to.equal(2);
   });
 
-  it("returns Infinity when dividing a non-zero number by 0", () => {
-    expect(divide(1, 0)).to.equal(Infinity);
+  it("divides a positive number with a negative number", () => {
+    expect(divide(10, -5)).to.equal(-2);
   });
 
-  it("returns 0 when 0 is divided by any non-zero number", () => {
-    expect(divide(0, 5)).to.equal(0);
+  it("converts string arguments to numbers before division", () => {
+    expect(divide("12", "4")).to.equal(3);
   });
-  */
 
-  it("returns NaN when dividing by zero", () => {
+  it("returns default value when both arguments are undefined", () => {
+    expect(divide(undefined, undefined)).to.equal(1); // Assuming default value is 1
+  });
+
+  it("returns the first argument when second argument is undefined", () => {
+    expect(divide(6, undefined)).to.equal(6);
+  });
+
+  it("returns the second argument when first argument is undefined", () => {
+    expect(divide(undefined, 3)).to.equal(3);
+  });
+
+  it("handles division by zero", () => {
     expect(divide(6, 0)).to.be.NaN;
+  });
+
+  it("returns NaN when dividing non-coercible strings", () => {
+    expect(divide("a", "b")).to.be.NaN;
   });
 });
