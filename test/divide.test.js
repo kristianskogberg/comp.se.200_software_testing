3ee1d2158d8b297923029c8c7726e504d2633760
Kristian Skogberg <k.skogberg98@gmail.com>
2023-11-22 11:41:48 +0200

    uncomment divide.js test

--- test/divide.test.js
+++ test/divide.test.js
@@ -3,10 +3,9 @@ import divide from "../src/divide.js";
 const expect = chai.expect;
 
 // NOTE: divide-function is incorrect, it divides the divisor with itself instead of using the divident
-/*
+
 describe("Divide", () => {
   it("divide a positive number with a positive number", () => {
     expect(divide(6, 2)).to.equal(3);
   });
 });
-*/
