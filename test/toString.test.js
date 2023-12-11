730e54b1f112bc7cb07d6b0187165a785bdda6ea
kristian_skogberg <kristian.skogberg@tuni.fi>
2023-12-11 11:03:00 +0200

    add more unit tests to filter.js and toString.js functions to achieve 100% code coverage for them

--- test/toString.test.js
+++ test/toString.test.js
@@ -1,25 +1,28 @@
-import chai from 'chai';
-import toString from '../src/toString.js';
+import chai from "chai";
+import toString from "../src/toString.js";
 const expect = chai.expect;
 
-
-describe('toString', function() {
-  it('should return the string for string inputs', function() {
-    expect(toString('abc')).to.equal('abc');
+describe("toString", function () {
+  it("should return the string for string inputs", function () {
+    expect(toString("abc")).to.equal("abc");
   });
 
-  it('should return the string for number inputs', function() {
-    expect(toString(123)).to.equal('123');
-    expect(toString(-0)).to.equal('-0');
+  it("should return the string for number inputs", function () {
+    expect(toString(123)).to.equal("123");
+    expect(toString(-0)).to.equal("-0");
   });
 
-  it('should return the string for array inputs', function() {
-    expect(toString([1, 2, 3])).to.equal('1,2,3');
+  it("should return the string for array inputs", function () {
+    expect(toString([1, 2, 3])).to.equal("1,2,3");
   });
 
-  it('should return the string for symbol inputs', function() {
-    const symbol = Symbol('symbol');
+  it("should return the string for symbol inputs", function () {
+    const symbol = Symbol("symbol");
     expect(toString(symbol)).to.equal(symbol.toString());
   });
+  it("should correctly convert arrays with various elements", function () {
+    const array = [1, "a", null, undefined, [2, "b", null], -0];
+    const expectedString = "1,a,,,[2,b,],-0";
+    expect(toString(array)).to.equal(expectedString);
+  });
 });
-
